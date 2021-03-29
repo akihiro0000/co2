06f950104d8dd1c130f16822d4614682ecf2ee20
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-03-29 09:55:17 +0900

    Update get.py

--- get.py
+++ get.py
@@ -228,7 +228,7 @@ class AirConditionMonitor:
                             
                         tim = '"timestamp":"'+datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S.%f')+'"'
                         co2 = '"' + "CO2[ppm]" + '"' + ":" + '"' + str(co2) + '"'
-			co2_device = '"' + "co2_device" + '"' + ":" + '"' + str(co2_address) + '"'
+                        co2_device = '"' + "co2_device" + '"' + ":" + '"' + str(co2_address) + '"'
                         mylist = [tim,bme_device,temp,pre,hum,co2_device,co2]
                         mystr = '{' + ','.join(map(str,mylist))+'}'
                         print(mystr)
