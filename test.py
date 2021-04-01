500d968c5e400ba75e85e388eb3a0eb9f7dcea6e
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-04-01 13:22:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -203,47 +203,16 @@ class AirConditionMonitor:
         else:
             return self.CO2_STATUS_TOO_HIGH
 
-    def execute(self):
-        
-        t0 = time.time()
-        while True:
-            if (time.time() - t0)>60 :
-		
-		#basic setting
-                tim = '"timestamp":"'+datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S.%f')+'"'
-		
-		#bme280
-                bme_device,temp,pre,hum = readData()
-		
-		#co2
-                out=0
-                try:
-                    if not self._ccs811.readData():
-                        co2,co2_address = self._ccs811.geteCO2()
-                        co2_status = self.status(co2)
-                        if co2_address=="None":	
-                            co2 = '"' + "CO2[ppm]" + '"' + ":" + '"' + str(0) + '"'
-			    out=1
-			else:
-			    co2 = '"' + "CO2[ppm]" + '"' + ":" + '"' + str(co2) + '"'
-                        co2_device = '"' + "co2_device" + '"' + ":" + '"' + str(co2_address) + '"'
-                        if co2_status != self.co2_status:
-                            self.co2_status = co2_status
-                except:
-                    co2 = '"' + "CO2[ppm]" + '"' + ":" + '"' + str(0) + '"'
-                    co2_device = '"' + "co2_device" + '"' + ":" + '"' + "None" + '"'
-                    out = 1
+    def execute(self):	
+	if not self._ccs811.readData():
+		co2,co2_address = self._ccs811.geteCO2()
+		co2_status = self.status(co2)
+	if co2_status != self.co2_status:
+	    self.co2_status = co2_status
+	
+	print(co2)
 
-	        #mqtt
-                mylist = [tim,bme_device,temp,pre,hum,co2_device,co2]
-                mystr = '{' + ','.join(map(str,mylist))+'}'
-                print(mystr)
-                #mqtt_client.publish("{}/{}".format("/demo",'car_count'), mystr)
-                t0 = time.time()
-                if out==1:
-                    break
-
-            sleep(1)
+        sleep(1)
 
 if __name__ == '__main__':
     #mqtt_client = mqtt.Client()
